538145d515a05bf1ed0e581fada408986c2fa473
Mohd. Arshul <arshulmohd123@gmail.com>
2018-05-23 03:26:34 +0530

    api call on button click

--- src/App.js
+++ src/App.js
@@ -1,28 +1,74 @@
 import React from 'react'
-import { graphql } from 'react-apollo'
+import { graphql , ApolloConsumer} from 'react-apollo'
 import { gql } from 'apollo-boost'
 
 
+const searchQuery =gql`
+query youtubeSearch($search_query: String!) {
+    search(search_query: $search_query){
+        id 
+        description 
+        channel 
+        title 
+        thumb_url
+    }
+}`;
+class Input extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            keyword : ''
+        };
+
+        this.updateInput = this.updateInput.bind(this);
+    }
+    updateInput(event){
+        this.setState({keyword : event.target.value})
+    }
+    render() {
+        return <div>
+            <div>
+            <input type="text" onChange={this.updateInput}/>
+                <ApolloConsumer>
+                    {client => (<button
+                        onClick={async () => {
+                            const { data } = await client.query({
+                                query: searchQuery,
+                                variables: { search_query: this.state.keyword }
+                            });
+                            console.log(data) // send this data to output
+                        }}
+                    >
+                        Click me!
+                    </button>
+                    )}
+                </ApolloConsumer>
+            </div>
+        </div>;
+    }
+}
+class Output extends React.Component{
+    render(){
+        return <div>
+
+        </div>
+    }
+}
 class  App extends React.Component {
   render() {
     return <div>
-      {this.props.data.loading === true ? "Loading" : this.props.data.search.map(data =>
-          <ul>
-            <li style={{fontWeight: 'bold'}}><a href={data.author}>{data.title}</a></li>
-          </ul>
-        )}
+        <Input/>
+        <Output/>
+          {/*{this.props.data.loading === true ? "Loading" : this.props.data.search.map(data =>*/}
+              {/*<ul>*/}
+                {/*<li key={data.id} style={{fontWeight: 'bold'}}><a href={"https://youtu.be/"+data.id}>{data.title}</a></li>*/}
+              {/*</ul>*/}
+            {/*)}*/}
     </div>;
   }
-} 
-const AppWithData = graphql(
-  gql`query{books{title author}}`,
-  {
-    options: {
-      variables: {
-        name: "tuts"
-      }
-    }
-  }
-)(App);
- 
-export default AppWithData;
+}
+// const AppWithData = graphql(searchQuery, {options: {variables: {search_query:"mkbhd"}}})(App);
+
+export default App;
+
--- src/index.js
+++ src/index.js
@@ -4,47 +4,17 @@ import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { ApolloProvider } from 'react-apollo'
-import { setContext } from 'apollo-link-context'
-import { graphql } from 'react-apollo'
-import gql from 'graphql-tag'
-import App from './App'
+import App  from './App'
 
 const client = new ApolloClient({
   link:new HttpLink({ uri: 'http://localhost:4000/graphql' }),
   cache: new InMemoryCache()
 });
 
-
-
 ReactDOM.render(
   <ApolloProvider client={client}>
-      <App />
+      <App/>
   </ApolloProvider>,
   document.getElementById('root')
 );
 
-
-// client.query({
-//   query: gql`{books { title author }}`,
-// }).then(console.log)
-
-var search_query = "carryminati";
-var xhr = new XMLHttpRequest();
-xhr.responseType = 'json';
-xhr.open("POST", "http://localhost:4000/graphql");
-xhr.setRequestHeader("Content-Type", "application/json");
-xhr.setRequestHeader("Accept", "application/json");
-xhr.onload = function () {
-    console.log('data returned:', xhr.response);
-}
-
-var query = `query youtubeSearch($search_query: String!) {
-  search(search_query: $search_query){id}
-}`;
-xhr.send(JSON.stringify({
-    query: query,
-    variables: {search_query:search_query},
-}));
-
- 
- 
