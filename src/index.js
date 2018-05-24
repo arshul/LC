ae7da6a7c64688a0e1581efe2c7c719baaecbc5d
Mohd. Arshul <arshulmohd123@gmail.com>
2018-05-24 16:23:01 +0530

    server domain chngd

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from 'react-apollo'
 import App  from './App'
 
 const client = new ApolloClient({
-  link:new HttpLink({ uri: 'http://localhost:4000/graphql' }),
+  link:new HttpLink({ uri: 'http://18.219.31.5:4000/graphql' }),
   cache: new InMemoryCache()
 });
 
